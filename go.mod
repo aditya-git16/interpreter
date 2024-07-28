2324cc86c4b09db50a93f08a078091b3bc6cd531
adi16an <es1221211@iitd.ac.in>
2024-07-28 22:15:58 +0530

    todo : continue with the lexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aditya-git16/interpreter
+
+go 1.21.3
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,5 +2,26 @@ package lexer
 
 import (
 	"testing"
-	"interpreter/token"
+	"github.com/aditya-git16/interpreter/token"
 )
+
+func TestNextToken(t *testing.T){
+	input := `=+(){},;`
+
+	test := []struct{
+		expectedType token.TokenType
+		expoeectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+}
